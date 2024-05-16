b9712056b6e621e783a5ebe678ae86db54bb847f
Kevin Liu <kliu0932@gmail.com>
2024-05-15 21:59:50 -0400

    force no italics

--- init.lua
+++ init.lua
@@ -786,6 +786,9 @@ require('lazy').setup({
     'catppuccin/nvim',
     name = 'catppuccin',
     priority = 1000, -- Make sure to load this before all the other start plugins.
+    opts = {
+      no_italic = true,
+    },
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
